bf37498e5837dc456f013c1da255a343a64faeb7
Vitali <v.yermakovich@edoktor24.pl>
2022-03-11 15:49:32 +0100

    add ingredient entity, create bidirectional connection between identity and recipe

---
+++ src/main/java/com/vermakovich/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,17 @@
+package com.vermakovich.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+}
