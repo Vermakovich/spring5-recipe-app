5ca215134ed12629f110910cb10a4ab4d8bc182c
Vitali <v.yermakovich@edoktor24.pl>
2022-03-11 15:37:12 +0100

    add notes and recipe entities

---
+++ src/main/java/com/vermakovich/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,17 @@
+package com.vermakovich.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+}
